6aa2b26e7c30427e4de0cc472ab0f20e83a4eea1
DwivediAmar <Amar.18266@stu.upes.ac.in>
2024-09-13 12:48:54 +0530

    Create PracticeEx1.py

---
+++ PracticeEx1.py
@@ -0,0 +1,10 @@
+
+# 1 write a program to take details from a student for ID-card and then print it in different lines.
+
+Name = input("Enter Your Full Name : ")
+dep = input("Enter Your Depertment : ")
+program = input("Enter Your Program Name : ")
+batch = input("Enter Your Batch Number : ")
+BOD = input("Enter Your Birth Date : ")
+
+print(Name,"//",dep,"//",program,"//",batch,"//",BOD )
